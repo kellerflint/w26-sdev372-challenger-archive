876fba39f2b3e87fa0864865d9025df648dbcb99
abdirashidexe <89213120+abdirashidexe@users.noreply.github.com>
2026-02-09 13:19:50 -0800

    Mock API now appears on GitHub

--- backend/server/index.js
+++ backend/server/index.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import sequelize from './db/db.js';
+// import sequelize from './db/db.js';
 import cors from 'cors'
 
 
@@ -10,9 +10,24 @@ const app = express();
 const PORT = 3001;
 
 app.use(cors());
-app.use("/matches", matchesRouter());       
-app.use("/players", playerRouter());   
+// app.use("/matches", matchesRouter());       
+// app.use("/players", playerRouter());   
 
+app.get("/", (req, res) => {
+    fetch("https://www.thesportsdb.com/api/v2/examples/all_sports.json", {
+        headers: {
+            Accept: "application/JSON"
+        }
+    })
+    .then(res => res.json())
+    .then(dataFromMyJSON => {
+        console.log(dataFromMyJSON.all[2].strSport),
+        res.send({
+            sport: dataFromMyJSON.all[2].strSport,
+            sportPic: dataFromMyJSON.all[3].strSportThumb
+         })
+    })
+})
 
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
